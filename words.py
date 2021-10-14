1770e6bf1b335df2779a9b5522d3ee6ccf30c165
James Porter <james@jamesporter.me>
2021-10-14 16:37:35 -0400

    a start

---
+++ words.py
@@ -0,0 +1,28 @@
+import random
+import gensim.downloader as api
+from blessings import Terminal
+
+def main(seed_word):
+    # print("Loading model...")
+    # model = api.load("glove-twitter-25")
+    term = Terminal()
+    curr_word = seed_word
+    used_words = [curr_word]
+    done = False
+    print("Starting...")
+    while not done:
+        candidates = [
+            word for (word, score)
+            in model.most_similar(curr_word)
+            if word not in used_words and word.isalpha()
+        ]
+        if not candidates:
+            done = True
+        else:
+            curr_word = candidates[0] #random.choice(candidates)
+            used_words.append(curr_word)
+        print(term.clear())
+        print(curr_word)
+
+if __name__ == "__main__":
+    main()
